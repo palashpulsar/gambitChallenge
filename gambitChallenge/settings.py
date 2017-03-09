1cbe52964c9654f1bf8f4387f1a9e16615e08e8d
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-03-09 17:29:51 +0200

    .

--- gambitChallenge/settings.py
+++ gambitChallenge/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # https://realpython.com/blog/python/asynchronous-tasks-with-django-and-celery/
