ffaa11ce09b302fd9d471218734757d6cfcdeaff
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-03-09 14:00:15 +0200

    Few more configurations were done for heroku ogin.

--- gambitChallenge/settings.py
+++ gambitChallenge/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # https://realpython.com/blog/python/asynchronous-tasks-with-django-and-celery/
